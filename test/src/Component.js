57cba12d6b389898fd3361527eee2ffa07ad912a
csm <csm009177@gmail.com>
2024-01-02 19:30:52 +0900

    hash change success

--- test/src/Component.js
+++ test/src/Component.js
@@ -1,52 +1,19 @@
 import { stateData, strData } from "./data.js";
 
-export function createElement(type, props, ...children) {
-  return { type, props, children };
-}
-
-export function render(virtualDom) {
-  if (typeof virtualDom === "string") {
-    return document.createTextNode(virtualDom);
+export const handleButtonClick = (inputValue) => {
+  const matchedItem = stateData.find((item) => item.text === inputValue);
+  
+  if (matchedItem) {
+    // 여기에서 원하는 로직을 구현할 수 있습니다.
+    window.location.hash = matchedItem.hash;
+    return {
+      status: "success",
+      matchedItem: matchedItem,
+    };
+  } else {
+    return {
+      status: "error",
+      message: "일치하는 이름을 찾을 수 없습니다.",
+    };
   }
-  const element = document.createElement(virtualDom.type);
-  if (virtualDom.props) {
-    for (const [key, value] of Object.entries(virtualDom.props)) {
-      element.setAttribute(key, value);
-    }
-  }
-  for (let i = 0; i < virtualDom.children.length; i++) {
-    const child = virtualDom.children[i];
-    element.appendChild(render(child));
-  }
-  return element;
-}
-
-export function hashCompo(index) {
-  const middle = document.getElementById("middle");
-  const middlebody = createElement(
-    "div",
-    { id: stateData[index].text, class: "middleCont" },
-    createElement(
-      "div",
-      { class: "top" },
-      createElement("h1", {}, stateData[index].text),
-      createElement("p", {}, strData[0].str1)
-    ),
-    createElement(
-      "div",
-      { class: "middle" },
-      createElement("h1", {}, stateData[index].text + " 장점"),
-      createElement("p", {}, strData[0].str2)
-    ),
-    createElement(
-      "div",
-      { class: "bottom" },
-      createElement("input", {
-        id: "inputTag",
-        placeholder: "write your name",
-      }),
-      createElement("div", {}, "")
-    )
-  );
-  middle.appendChild(render(middlebody));
-}
+};
--- test/src/MiddleComponent.js
+++ test/src/MiddleComponent.js
@@ -1,6 +1,6 @@
-import React, {useState } from "react";
-import { stateData,strData } from "./data.js";
-import {createElement, render, hashCompo} from './Component.js'
+import React, { useState } from "react";
+import { stateData, strData } from "./data.js";
+import { createElement, render, hashCompo } from './Component.js';
 
 
 const MiddleComponent = () => {
@@ -12,7 +12,33 @@ const MiddleComponent = () => {
 
   const handleButtonClick = () => {
     const matchedItem = stateData.find((item) => item.text === inputValue);
+    
     if (matchedItem) {
+      const boardHead = document.getElementById("boardHead");
+      const boardBody = document.getElementById("boardBody");
+
+      
+      // 기존의 h1 요소가 있다면 삭제
+      const existingHeadTitle = boardHead.querySelector("h1");
+      if (existingHeadTitle) {
+        boardBody.innerHTML = ""
+        boardHead.innerHTML = ""
+      }
+      
+      // 새로운 h1 요소 생성 및 추가
+      const creHeadTitle = document.createElement("h1");
+      const creHeadContent = document.createElement("p");
+      const creBodyTitle = document.createElement("h1");
+      const creBodyContent = document.createElement("p");
+      creHeadTitle.innerHTML = matchedItem.text;
+      creHeadContent.innerHTML = strData[0].str1;
+      creBodyTitle.innerHTML = matchedItem.text;
+      creBodyContent.innerHTML = strData[0].str2;
+      boardHead.appendChild(creHeadTitle);
+      boardHead.appendChild(creHeadContent)
+      boardBody.appendChild(creBodyTitle);
+      boardBody.appendChild(creBodyContent)
+
       window.location.hash = matchedItem.hash;
     } else {
       alert("일치하는 이름을 찾을 수 없습니다.");
@@ -30,9 +56,7 @@ const MiddleComponent = () => {
           value={inputValue}
           onChange={handleInputChange}
         />
-        <button id="button" onClick={handleButtonClick}>
-          show
-        </button>
+        <button id="button" onClick={handleButtonClick}>show</button>
       </div>
     </div>
   );
